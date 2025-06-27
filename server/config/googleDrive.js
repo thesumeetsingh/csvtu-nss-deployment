3001b6693690c9dfd4d44f1e451c4b39585676ef
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-06-27 15:13:49 +0530

    modified credentials

--- server/config/googleDrive.js
+++ server/config/googleDrive.js
@@ -2,7 +2,15 @@ const { google } = require('googleapis');
 const fs = require('fs');
 require('dotenv').config();
 
-const credentials = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
+// const credentials = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
+
+let credentials;
+try {
+  credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS || '{}');
+} catch (error) {
+  console.error('Error parsing GOOGLE_CREDENTIALS:', error);
+  throw new Error('Invalid GOOGLE_CREDENTIALS format');
+}
 
 const auth = new google.auth.GoogleAuth({
   credentials: credentials,
