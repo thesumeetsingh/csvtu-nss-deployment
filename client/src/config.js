f6de8a05c2a8ad1b00c82981e063e4fd91e61d45
thesumeetsingh <sumeetsingh9752818473@gmail.com>
2025-06-28 14:36:13 +0530

    ready to deploy version 10

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'https://csvtu-nss-deployment.onrender.com/api';
+const API_BASE_URL ='https://csvtu-nss-deployment.onrender.com/api';
 
 export default API_BASE_URL;
+
+//import.meta.env.VITE_API_URL ||
